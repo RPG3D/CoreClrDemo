f0d1439db5edd61d7f13a345f349091c7a065e28
admin <>
2025-11-29 21:20:32 +0800

    fix pass native function into managed code

--- ManagedDemo/ManagedClass.cs
+++ ManagedDemo/ManagedClass.cs
@@ -5,7 +5,7 @@
 
         public static void PrintMessage(string InMsg)
         {
-            System.Console.WriteLine("PrintMessage in C#, " + InMsg);
+            System.Console.WriteLine("Managed: PrintMessage in C#, " + InMsg);
         }
 
         public static int Add(int a, int b)
@@ -15,22 +15,26 @@
 
 
         // 使用 unmanaged 函数指针，因为指向的是 C++ 原生函数
-        public static delegate* unmanaged[Cdecl]<int, int, int> NativeAddPtr;
+        public static delegate* unmanaged<int, int, int> NativeAddPtr;
         
         // 托管方法，通过 Mono runtime 被 C++ 调用
-        public static void RegisterNativeFunction(IntPtr nativeFunctionPtr)
+        public static void RegisterNativeFunction(void* nativeFunctionPtr)
         {
             // 将传入的 IntPtr 转换为 unmanaged 函数指针
-            NativeAddPtr = (delegate* unmanaged[Cdecl]<int, int, int>)nativeFunctionPtr;
+            NativeAddPtr = (delegate* unmanaged<int, int, int>)nativeFunctionPtr;
         }
 
         // 另一个方法，用于后续调用原生函数
         public static int CallNativeAdd(int a, int b)
         {
             if (NativeAddPtr == null)
-                throw new InvalidOperationException("Native function not registered");
-                
-            return NativeAddPtr(a, b);
+            {
+                System.Console.WriteLine("Managed: Native function not registered");
+                throw new InvalidOperationException("Managed: Native function not registered");
+            }
+            int ret = NativeAddPtr(a, b);
+            System.Console.WriteLine("Managed: Native function called " + ret);
+            return ret;
         }
     }
 }
--- MonoAppDemo/src/MonoAppDemo.cpp
+++ MonoAppDemo/src/MonoAppDemo.cpp
@@ -22,7 +22,7 @@ int Add(int a, int b)
 
 int main()
 {
-    std::cout << "Hello Mono App(native)!\n";
+    std::cout << "Native: Hello Mono App!\n";
 	
 #if _WIN32
 	mono_set_dirs("D:/Code/MonoSDK/runtime", "");
@@ -42,6 +42,7 @@ int main()
 	
 	MonoClass* ManagedClass = mono_class_from_name(image, "ManagedDemo", "ManagedClass");
 
+	if(1)
 	{
 		MonoMethod* MsgMethod = mono_class_get_method_from_name(ManagedClass, "PrintMessage", 1);
 		// 准备参数
@@ -55,30 +56,39 @@ int main()
 
 	}
 
+	if(1)
 	{
 		MonoMethod* addMethod = mono_class_get_method_from_name(ManagedClass, "Add", 2);
 		AddFunction addFunc = (AddFunction)mono_method_get_unmanaged_thunk(addMethod);
 		int result = addFunc(5, 7); 
-		std::cout << "Result of Add(5, 7): " << result << std::endl;	
+		std::cout << "Native: Result of Add(5, 7): " << result << std::endl;	
 	}
 
+	if(1)
 	{
-		MonoMethod* method = mono_class_get_method_from_name(ManagedClass, "RegisterFunctionPtr", 1);
-		void* args[1];
-		args[0] = (void*)(&Add);
-		//mono_runtime_invoke(method, nullptr, args, nullptr);
+		MonoMethod* method = mono_class_get_method_from_name(ManagedClass, "RegisterNativeFunction", 1);
+		if(method)
+		{
+			void* args[1];
+			args[0] = (void*)(&Add);
+			mono_runtime_invoke(method, nullptr, args, nullptr);
+		}
+		else
+		{
+			std::cout << "Native: failed to RegisterNativeFunction" << std::endl;	
+		}
 
-		// MonoMethod* method2 = mono_class_get_method_from_name(ManagedClass, "CallNativeAdd", 2);
-		// void* args2[2];
-        // int a = 15, b = 25;
-        // args2[0] = &a;
-        // args2[1] = &b;
-        // MonoObject* exception = nullptr;
-        // MonoObject* result = mono_runtime_invoke(method, nullptr, args2, &exception); 
-        // if (!exception && result) {
-        //     int addResult = *(int*)mono_object_unbox(result);
-        //     printf("Managed call to native Add: %d + %d = %d\n", a, b, addResult);
-        // }
+		MonoMethod* method2 = mono_class_get_method_from_name(ManagedClass, "CallNativeAdd", 2);
+		void* args2[2];
+        int a = 15, b = 25;
+        args2[0] = &a;
+        args2[1] = &b;
+        MonoObject* exception = nullptr;
+        MonoObject* result = mono_runtime_invoke(method2, nullptr, args2, &exception); 
+        if (!exception && result) {
+            int addResult = *(int*)mono_object_unbox(result);
+            printf("Native: Managed call to native Add: %d + %d = %d\n", a, b, addResult);
+        }
 	}
 
 	mono_jit_cleanup(domain);
