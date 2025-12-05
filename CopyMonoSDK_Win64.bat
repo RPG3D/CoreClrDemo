cdf6357a30a56e192fbdae77d952447156b484e3
admin <>
2025-12-06 00:39:16 +0800

    1 fix copy mono sdk script 2 add mono dylib

--- CopyMonoSDK_Win64.bat
+++ CopyMonoSDK_Win64.bat
@@ -1,15 +1,15 @@
 
-rem DotNET/src/mono/
+rem place at DotNet source root directory
 
-set SrcDir=%~dp0../../artifacts
-set DstDir=D:/Code/MonoSDK_Temp
+set SrcDir=%~dp0/artifacts
+set DestDir=D:/Code/MonoSDK_Temp
 
 set BuildType=Debug
 
-del /Q "%DstDir%"
+del /Q "%DestDir%"
 
-xcopy "%SrcDir%/obj/mono/windows.x64.%BuildType%/out/include/mono-2.0" "%DstDir%/include" /E /I /Y
-xcopy "%SrcDir%/obj/mono/windows.x64.%BuildType%/out/lib" "%DstDir%/lib" /E /I /Y
-xcopy "%SrcDir%/obj/mono/windows.x64.%BuildType%/out/bin" "%DstDir%/bin" /E /I /Y
-xcopy "%SrcDir%/bin/runtime/net10.0-windows-%BuildType%-x64" "%DstDir%/runtime" /E /I /Y
-xcopy "%SrcDir%/bin/mono/windows.x64.%BuildType%/IL" "%DstDir%/runtime" /E /I /Y
+xcopy "%SrcDir%/obj/mono/windows.x64.%BuildType%/out/include/mono-2.0" "%DestDir%/include" /E /I /Y
+xcopy "%SrcDir%/obj/mono/windows.x64.%BuildType%/out/lib" "%DestDir%/lib" /E /I /Y
+xcopy "%SrcDir%/obj/mono/windows.x64.%BuildType%/out/bin" "%DestDir%/bin" /E /I /Y
+xcopy "%SrcDir%/bin/runtime/net10.0-windows-%BuildType%-x64" "%DestDir%/runtime" /E /I /Y
+xcopy "%SrcDir%/bin/mono/windows.x64.%BuildType%/IL" "%DestDir%/runtime" /E /I /Y
